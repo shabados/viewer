adce989b2db7d3296ef997c07a4d2f07f313e30d
Harjot Singh <harjot@harkul.com>
2019-01-31 04:30:08 +0000

    feat(frontend): add dev-only proxying to backend

--- frontend/src/lib/consts.js
+++ frontend/src/lib/consts.js
@@ -1,11 +1,6 @@
-// API Server
-const { NODE_ENV } = process.env
-const { location: { hostname, port } } = window
-export const API_HOST = hostname
-export const API_PORT = NODE_ENV === 'production' ? port : 52525
-export const API_URL = `http://${API_HOST}:${API_PORT}`
+const { location: { origin } } = window
 
 // API Routes
-export const API_ROOT = `${API_URL}/api`
+export const API_ROOT = `${origin}/api`
 export const SOURCES_API = `${API_ROOT}/sources`
 export const PAGE_API = `${API_ROOT}/source`

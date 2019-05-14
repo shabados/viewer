cc83736c074a3e41f7a0cadd4fe32bf1aa91fda0
Bhajneet S.K <bhajneet@gmail.com>
2019-05-14 14:42:58 -0400

    remove assignee from new github issues

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -3,7 +3,6 @@ import newGithubIssueUrl from 'new-github-issue-url'
 export const issueUrl = ( { id, gurmukhi, page, nameEnglish } ) => newGithubIssueUrl( {
   user: 'ShabadOS',
   repo: 'Database',
-  assignee: 'Sarabveer',
   labels: [ 'correction', nameEnglish ],
   title: `${id}`,
   body: `

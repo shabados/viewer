5110fd42e7f0589f2d0fe1c2df7d16968da40dba
Harjot Singh <harjot@harkul.com>
2019-06-18 02:21:03 +0100

    fix(frontend): allow holding down a registered react hotkey

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { configure } from 'react-hotkeys'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCaretLeft, faCaretRight, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
 
 import App from './App'
 import './index.css'
 
+configure( {
+  // Allow holding down a key
+  ignoreRepeatedEventsWhenKeyHeldDown: false,
+} )
+
 // Import font-awesome icons
-[ faCaretLeft, faCaretRight, faExclamationCircle ].forEach( icon => library.add( icon ) )
+;[ faCaretLeft, faCaretRight, faExclamationCircle ].forEach( icon => library.add( icon ) )
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) )

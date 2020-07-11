2a2a9a5ac788a0cb94bec79638dae44db069b278
saihaj <44710980+saihaj@users.noreply.github.com>
2020-07-11 15:07:11 -0500

    refactor: rename function name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   componentDidMount() {
     this.loadSources()
-    this.localDbVersion()
+    this.loadDbVersion()
   }
 
   loadSources = () => fetch( SOURCES_API )
@@ -25,35 +25,35 @@ class App extends Component {
     .then( sources => this.setState( { sources } ) )
     .catch( err => this.setState( { err } ) )
 
-  localDbVersion = () => fetch( DB_VERSION )
-    .then( res => res.json() )
-    .then( version => this.setState( { version } ) )
-    .catch( err => this.setState( { err } ) )
+    loadDbVersion = () => fetch( DB_VERSION )
+      .then( res => res.json() )
+      .then( version => this.setState( { version } ) )
+      .catch( err => this.setState( { err } ) )
 
-  render() {
-    const { sources, err, version } = this.state
-    const positions = getPositions()
+    render() {
+      const { sources, err, version } = this.state
+      const positions = getPositions()
 
-    return (
-      <Router>
-        <div className="app">
-          <Route exact path="/" render={() => <Home err={err} sources={sources} version={version} positions={positions} />} />
-          <Route
-            path="/sources/:source/page/:page/line/:line"
-            render={( { match: { params: { page, source, line } } } ) => (
-              <SourcePage
-                page={page}
-                source={source}
-                line={line}
-                {...sources.find( ( { id } ) => id === +source )}
-              />
-            )
+      return (
+        <Router>
+          <div className="app">
+            <Route exact path="/" render={() => <Home err={err} sources={sources} version={version} positions={positions} />} />
+            <Route
+              path="/sources/:source/page/:page/line/:line"
+              render={( { match: { params: { page, source, line } } } ) => (
+                <SourcePage
+                  page={page}
+                  source={source}
+                  line={line}
+                  {...sources.find( ( { id } ) => id === +source )}
+                />
+              )
             }
-          />
-        </div>
-      </Router>
-    )
-  }
+            />
+          </div>
+        </Router>
+      )
+    }
 }
 
 export default App

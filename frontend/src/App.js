30492b5c99b7e1c8296bf29588282fff63f8f546
saihaj <44710980+saihaj@users.noreply.github.com>
2020-07-28 16:07:52 -0500

    refactor(frontend): make app functional component to use hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import { getPositions } from './lib/utils'
@@ -8,52 +8,46 @@ import SourcePage from './components/SourcePage'
 
 import './App.css'
 
-class App extends Component {
-  state = {
-    sources: [],
-    err: null,
-    dbVersion: '',
-  }
+const App = () => {
+  const [ sources, setSources ] = useState( [] )
+  const [ err, setErr ] = useState( null )
+  const [ dbVersion, setDbVersion ] = useState( '' )
+  const positions = getPositions()
 
-  componentDidMount() {
-    this.loadSources()
-    this.loadDbVersion()
-  }
-
-  loadSources = () => fetch( SOURCES_API )
+  const loadSources = () => fetch( SOURCES_API )
     .then( res => res.json() )
-    .then( sources => this.setState( { sources } ) )
-    .catch( err => this.setState( { err } ) )
+    .then( sources => setSources( sources ) )
+    .catch( err => setErr( err ) )
 
-    loadDbVersion = () => fetch( DB_VERSION )
-      .then( res => res.json() )
-      .then( dbVersion => this.setState( { dbVersion } ) )
-      .catch( err => this.setState( { err } ) )
+  const loadDbVersion = () => fetch( DB_VERSION )
+    .then( res => res.json() )
+    .then( dbVersion => setDbVersion( dbVersion ) )
+    .catch( err => setErr( err ) )
 
-    render() {
-      const { sources, err, dbVersion } = this.state
-      const positions = getPositions()
+  useEffect( () => {
+    loadSources()
+    loadDbVersion()
+  }, [] )
 
-      return (
-        <Router>
-          <div className="app">
-            <Route exact path="/" render={() => <Home err={err} sources={sources} dbVersion={dbVersion} positions={positions} />} />
-            <Route
-              path="/sources/:source/page/:page/line/:line"
-              render={( { match: { params: { page, source, line } } } ) => (
-                <SourcePage
-                  page={page}
-                  source={source}
-                  line={line}
-                  {...sources.find( ( { id } ) => id === +source )}
-                />
-              )
-            }
+  return (
+    <Router>
+      <div className="app">
+        <Route exact path="/" render={() => <Home err={err} sources={sources} dbVersion={dbVersion} positions={positions} />} />
+        <Route
+          path="/sources/:source/page/:page/line/:line"
+          render={( { match: { params: { page, source, line } } } ) => (
+            <SourcePage
+              page={page}
+              source={source}
+              line={line}
+              {...sources.find( ( { id } ) => id === +source )}
             />
-          </div>
-        </Router>
-      )
-    }
+          )
+            }
+        />
+      </div>
+    </Router>
+  )
 }
 
 export default App

78a13c90ccee54abfff5cb19aafa1d90b72d976d
Harjot <harjot@harkul.com>
2021-10-25 20:28:41 +0100

    feat(frontend): render the version via build environment variable

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,6 +4,7 @@ import { arrayOf, string, number, shape } from 'prop-types'
 import Loader from './Loader'
 import Error from './Error'
 import logo from '../media/logo.svg'
+import { version } from '../lib/consts'
 
 import './Home.css'
 
@@ -13,13 +14,18 @@ const Home = ( { err, sources, positions, dbVersion } ) => (
   <div className="home">
     <section className="introduction">
       <img className="logo" src={logo} alt="Shabad OS Logo" />
+
       <h1>Viewer</h1>
-      <h4>{ !dbVersion ? 'Database Version loading...' : `Database Version ${dbVersion}`}</h4>
+      <h3>{`v${version}`}</h3>
+
+      <h4>{ !dbVersion ? 'Database Version loading...' : `Database Version v${dbVersion}`}</h4>
+
       <div className="links">
         <a href="https://youtu.be/YLtOxh5k7aw" className="button">Tutorial Video</a>
         <a href="https://tutorials.shabados.com/tutorials/1.0.0/database-viewer/proofreading.html" className="button">Learn to Proofread</a>
       </div>
     </section>
+
     <section className="sources">
       {err && <Error err={err} />}
       {!( sources || err ) && <Loader />}
